44c17e90dd238bea34e30fb870cfbaaaf350a2c5
moizkhan90 <77809055+moizkhan90@users.noreply.github.com>
2024-10-29 21:37:41 -0700

    Add files via upload

---
+++ Searching.In.Unsorted.java.py
@@ -0,0 +1,28 @@
+public class SearchElement {
+    // Function to implement search operation
+    static int findElement(int arr[], int n, int key) {
+        for (int i = 0; i < n; i++) {
+            if (arr[i] == key) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    // Driver's code
+    public static void main(String[] args) {
+        int arr[] = {12, 34, 10, 6, 40};
+        int n = arr.length;
+
+        // Using a last element as search element
+        int key = 40;
+        
+        // Function call
+        int position = findElement(arr, n, key);
+        if (position == -1) {
+            System.out.println("Element not found");
+        } else {
+            System.out.println("Element found at position: " + (position + 1));
+        }
+    }
+}
